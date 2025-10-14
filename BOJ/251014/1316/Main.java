3aedd831319a113b8161cc5a0b7c572bf24df37a
Doo Young <chlendyd7@naver.com>
2025-10-14 17:57:12 +0900

    251014

--- BOJ/251014/1316/Main.java
+++ BOJ/251014/1316/Main.java
@@ -5,5 +5,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+
+    }
+
+    static boolean isGroupWord(String word) {
+        boolean[] check = new boolean[26];
+        char prev = 0;
+
+        for (int i=0; i<word.length(); i++) {
+            
+        }
+
+        return true;
     }
 }
